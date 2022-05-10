d48bf7191ecf382a8e4b2c875baef291efcb247f
Aline <alinej.capucho@gmail.com>
2022-05-10 07:28:03 -0300

    feat: cholesky dec. tech debt: change fwdSub and retSub so they can be reused in cholesk as well as lu

--- src/matrixLib.py
+++ src/matrixLib.py
@@ -1,16 +1,17 @@
 import numpy as np
+from math import sqrt
 
 def fwdSub(matrix, vector):
   y = np.zeros([vector.size], dtype=float)
   y[0] = vector[0]
-  for i in range(1, vector.size, 1):
+  for i in range (1, vector.size, 1):
     y[i] = (vector[i] - sum((y[j]*matrix[i][j]) for j in range(0, i)))
   return y
 
 def retSub(matrix, vector):
   x = np.zeros([vector.size], dtype=float)
   x[vector.size-1] = vector[vector.size-1]/matrix[vector.size-1][vector.size-1]
-  for i in range(vector.size - 2, -1, -1):
+  for i in range (vector.size - 2, -1, -1):
     x[i] = (vector[i] - sum((x[j]*matrix[i][j]) for j in range(vector.size-1, i, -1)))/matrix[i][i]
   return x
 
@@ -23,13 +24,27 @@ def luDec(matrix, vector):
         matrix[l][j] = matrix[l][j]-(matrix[l][k]*matrix[k][j])
 
   y = fwdSub(matrix, vector)
-  print(y)
   x = retSub(matrix, y)
-  print(x)
   return x
 
 def choleskyDec(matrix, vector):
-  print('')
+  lMatrix = np.zeros([matrix[0].size,matrix[0].size], dtype=float)
+  for i in range(0, matrix[0].size, 1):
+    lMatrix[i][i] = sqrt(matrix[i][i] - sum((pow(lMatrix[i][k], 2)) for k in range(0, i)))
+    for j in range (i+1, matrix[0].size, 1):
+      lMatrix[j][i] = (1/(lMatrix[i][i]))*(matrix[i][j] - sum((lMatrix[i][k]*lMatrix[j][k]) for k in range(0, i)))
+
+  y = np.zeros([vector.size], dtype=float)
+  y[0] = vector[0]/lMatrix[0][0]
+  for i in range (1, vector.size, 1):
+    y[i] = (vector[i] - sum((y[j]*lMatrix[i][j]) for j in range(0, i)))/lMatrix[i][i]
+
+  x = np.zeros([y.size], dtype=float)
+  x[y.size-1] = y[y.size-1]/lMatrix[y.size-1][y.size-1]
+  for i in range (y.size - 2, -1, -1):
+    x[i] = (y[i] - sum((x[j]*lMatrix[j][i]) for j in range(y.size-1, i, -1)))/lMatrix[i][i]
+
+  return x
 
 def itrJacobi(matrix, vector):
   print('')
