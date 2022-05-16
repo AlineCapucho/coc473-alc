469ac3d488815c0a83de7ffe4e1d24c587251256
Aline <alinej.capucho@gmail.com>
2022-05-16 20:42:50 -0300

    Update the user call function and the functions returns to write on file

--- src/matrixLib.py
+++ src/matrixLib.py
@@ -1,9 +1,9 @@
 import numpy as np
-from math import sqrt
+from math import sqrt, sin, cos, atan, pi
 
 np.seterr(all='raise')
 
-def fwdSub(matrix, vector):
+def __fwdSub(matrix, vector):
   """"Calculates the foward substitution in LU decomposition"""
   y = np.zeros([vector.size], dtype=float)
   y[0] = vector[0]
@@ -11,7 +11,7 @@ def fwdSub(matrix, vector):
     y[i] = (vector[i] - sum((y[j]*matrix[i][j]) for j in range(0, i)))
   return y
 
-def retSub(matrix, vector):
+def __retSub(matrix, vector):
   """"Calculates the retro substitution in LU decomposition"""
   x = np.zeros([vector.size], dtype=float)
   x[vector.size-1] = vector[vector.size-1]/matrix[vector.size-1][vector.size-1]
@@ -19,14 +19,14 @@ def retSub(matrix, vector):
     x[i] = (vector[i] - sum((x[j]*matrix[i][j]) for j in range(vector.size-1, i, -1)))/matrix[i][i]
   return x
 
-def detTrg(matrix):
+def __detTrg(matrix):
   """Calculates the determinant for an upper or lower triangular matrix"""
   det = 0
   for i in range(0, matrix[0].size, 1):
     det += matrix[i][i]
   return det
 
-def diagDom(matrix):
+def __diagDom(matrix):
   """"Calculates wether a given matrix is diagonally dominant"""
   sumL = 0
   sumC = 0
@@ -41,20 +41,32 @@ def diagDom(matrix):
     sumC = 0
   return True
 
-def eucNorm(vector):
+def __symmetric(matrix):
+  if( matrix.any() != np.transpose(matrix).any):
+    return False
+  return True
+
+def __eucNorm(vector):
   """Calculates the euclidian norm of a given matrix"""
   eucNorm = 0
   for i in range(0, vector.size, 1):
       eucNorm += pow(vector[i], 2)
   return(sqrt(eucNorm))
 
-def infNorm(vector):
+def __infNorm(vector):
   """Calculates the infinite norm of a given matrix"""
   infNorm = 0
   for i in range(0, vector.size, 1):
       if abs(vector[i]) > infNorm:
         infNorm = abs(vector[i])
   return(infNorm)
+
+def __diagonal(matrix, tolm):
+  for i in range(0, matrix[0].size, 1):
+    for j in range(0, matrix[0].size, 1):
+      if j!=i and matrix[i][j]>tolm:
+        return False
+  return True
 
 def luDec(matrix, vector, detCod):
   """Solves a given linear equation system through the LU Decomposition
@@ -67,16 +79,21 @@ def luDec(matrix, vector, detCod):
         for l in range (k+1, matrix[0].size, 1):
           matrix[l][j] = matrix[l][j]-(matrix[l][k]*matrix[k][j])
   except:
-    return 'Unable to complete LU decomposition. Cause: matrix is singular.'
+    return ['Unable to complete LU decomposition. Matrix is singular.']
   else:
-    y = fwdSub(matrix, vector)
-    x = retSub(matrix, y)
-    det = 0
+    y = __fwdSub(matrix, vector)
+    x = __retSub(matrix, y)
+    result = [
+      "X vector: " + str(x)
+    ]
     if detCod>0:
-      det = detTrg(matrix)
-      print(det)
-    print(x)
+      det = __detTrg(matrix)
+      result.append(
+        "Determinant: " + str(det)
+      )
 
+  return result
+  
 def choleskyDec(matrix, vector):
   """Solves a given linear equation system through the Cholesky Decomposition"""
   try:
@@ -86,7 +103,7 @@ def choleskyDec(matrix, vector):
       for j in range (i+1, matrix[0].size, 1):
         lMatrix[j][i] = (1/(lMatrix[i][i]))*(matrix[i][j] - sum((lMatrix[i][k]*lMatrix[j][k]) for k in range(0, i)))
   except:
-    return('Unable to complete Cholesky Decomposition. Cause: matrix is not symmetric positive definite.')
+    return ['Unable to complete Cholesky Decomposition. Matrix is not symmetric positive definite.']
   else:
     y = np.zeros([vector.size], dtype=float)
     y[0] = vector[0]/lMatrix[0][0]
@@ -98,35 +115,53 @@ def choleskyDec(matrix, vector):
     for i in range (y.size - 2, -1, -1):
       x[i] = (y[i] - sum((x[j]*lMatrix[j][i]) for j in range(y.size-1, i, -1)))/lMatrix[i][i]
 
-    return x
+    result = [
+      "X vector: " + str(x)
+    ]
+
+    return result
 
 def itrJacobi(matrix, vector, tolm):
   """Solves a given linear equation system through the Jacobi Iteration"""
-  if (diagDom(matrix) == False):
-    print('Matrix is not diagonal dominant. Convergence not guaranteed.')
   try:
+    result = []
+    if (__diagDom(matrix) == False):
+      result.append('Matrix is not diagonal dominant. Convergence not guaranteed.')
+
     x0 = np.zeros([vector.size], dtype=float)
     x = np.zeros([vector.size], dtype=float)
     cont = 0
     res = 1
+
     while(res > tolm):
       for i in range(0, vector.size, 1):
         x[i] = (vector[i] - sum((matrix[i][j]*x0[j] if j!=i else 0) for j in range (0, vector.size, 1)))/matrix[i][i]
-      res = eucNorm(np.subtract(x, x0))/eucNorm(x)
+      res = __eucNorm(np.subtract(x, x0))/__eucNorm(x)
       x0 = np.copy(x)
       cont += 1
-    return x
-  except:
-    print('Unable to complete operation. Could not converge.')
+  
+    result.append(
+      'X vector: ' + str(x)
+    )
+    result.append(
+      'Number of iterations: ' + str(cont)
+    )
+    result.append(
+      'Residue: ' + str(res)
+    )
 
-#after adding eigenvalues can calculate if
-#matrix is Symetric Positive Defined
-#and use condition here
+  except:
+    result.append('Unable to complete operation. Could not converge.')
+  
+  return result
+
 def itrGaussSeidel(matrix, vector, tolm):
   """Solves a given linear equation system through the Gauss-Seidel Iteration"""
-  if (diagDom(matrix) == False):
-    print('Matrix is not diagonal dominant. Convergence not guaranteed.')
   try:
+    result = []
+    if (__diagDom(matrix) == False):
+      result.append('Matrix is not diagonal dominant. Convergence not guaranteed.')
+
     x0 = np.zeros([vector.size], dtype=float)
     x = np.zeros([vector.size], dtype=float)
     cont = 0
@@ -134,36 +169,95 @@ def itrGaussSeidel(matrix, vector, tolm):
     while(res > tolm):
       for i in range(0, vector.size, 1):
         x[i] = (vector[i] - sum((matrix[i][j]*x[j] if j!=i else 0) for j in range (0, vector.size, 1)))/matrix[i][i]
-      res = eucNorm(np.subtract(x, x0))/eucNorm(x)
+      res = __eucNorm(np.subtract(x, x0))/__eucNorm(x)
       x0 = np.copy(x)
       cont += 1
-    return x
+    
+    result.append(
+      'X vector: ' + str(x)
+    )
+    result.append(
+      'Number of iterations: ' + str(cont)
+    )
+    result.append(
+      'Residue: ' + str(res)
+    )
+
   except:
-    print('Unable to complete operation. Could not converge.')
+    result.append(
+      'Unable to complete operation. Could not converge.'
+    )
+  
+  return result
 
 def powerMet(matrix, tolm):
   x = np.full(matrix[0].size, 1)
   x[0] = 1
-  ups0 = infNorm(x)
+  ups0 = __infNorm(x)
   cont = 0
   res = 1
+  result = []
   
   while(res > tolm):
     y = np.matmul(matrix, x)
-    ups1 = infNorm(y)
+    ups1 = __infNorm(y)
     res = abs(ups1 - ups0)/ups1
     if res>tolm:
       cont += 1
       x = y/ups1
       ups0 = ups1
   
-  print(x, cont, res)
-  return x
+  result = [
+    'X vector:' + str(x),
+    'Number of iterations: ' + str(cont),
+    'Residue: ' + str(res)
+  ]
 
-
+  return result
 
 def jacobiMet(matrix, tolm):
-  print('')
+  if(__symmetric(matrix) == False):
+    return['Could not complete Jacobi Method. Matrix is not symmetric.']
+  else:
+    pMatrix = np.zeros([matrix[0].size, matrix[0].size], dtype=float)
+    aMatrix = np.copy(matrix)
+    cont = 0
+    max = 0
+    line = 0
+    column = 0
+    o = 0
+
+    while(__diagonal(pMatrix, tolm) == False):
+      cont += 1
+
+      for i in range(0, aMatrix[0].size, 1):
+        for j in range(0, aMatrix[0].size, 1):
+          if j!=i and abs(aMatrix[i][j])>max:
+            max = abs(aMatrix[i][j])
+            line = i
+            column = j
+
+      for i in range(0, aMatrix[0].size, 1):
+        for j in range(0, aMatrix[0].size, 1):
+          if j!=i and abs(aMatrix[i][j])>max:
+            max = abs(aMatrix[i][j])
+            line = i
+            column = j
+        pMatrix[i][i] = 1
+      
+      if(aMatrix[line][line] != aMatrix[column][column]):
+        o = (atan*(aMatrix[line][column]/(aMatrix[line][line] - aMatrix[column][column])))/2
+      else:
+        o = pi/4
+
+      pMatrix[line][column] = sin(o)
+      pMatrix[column][line] = -sin(o)
+      pMatrix[line][line] = cos(o)
+      pMatrix[column][column] = cos(o)
+
+      apmult = np.matmul()
+      if(__diagonal(pMatrix, tolm) == False):
+        aMatrix = pMatrix
 
 def interpolation():
   print('')
--- src/readLib.py
+++ src/readLib.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matrixLib as mtx
 
-def readFileT1(file):
+def runOperation(readFile, writeFile, task):
+  if task == 1:
+    results = readTask1(readFile)
+  elif task == 2:
+    results = readTask2(readFile)
+  elif task == 3:
+    results = readTask3(readFile)
+  write(writeFile, results)
+
+def readTask1(file):
   with open(file, 'r') as f:
     order = int(f.readline())
     icod = int(f.readline())
@@ -19,17 +28,19 @@ def readFileT1(file):
       tolm = float(f.readline())
 
     if(icod==1):
-      mtx.luDec(aMatrix, bVector, idet)
+      result = mtx.luDec(aMatrix, bVector, idet)
     elif(icod==2):
-      mtx.choleskyDec(aMatrix, bVector)
+      result = mtx.choleskyDec(aMatrix, bVector)
     elif(icod==3):
-      mtx.itrJacobi(aMatrix, bVector, tolm)
+      result = mtx.itrJacobi(aMatrix, bVector, tolm)
     elif(icod==4):
-      mtx.itrGaussSeidel(aMatrix, bVector, tolm)
+      result = mtx.itrGaussSeidel(aMatrix, bVector, tolm)
     if(idet>0 and icod in [2,3,4]):
-      print("Warning: cannot calculate determinant.")
+      result.append("Warning! Could not calculate determinant.")
+  
+  return result
 
-def readFileT2(file):
+def readTask2(file):
   with open(file, 'r') as f:
     order = int(f.readline())
     icod = int(f.readline())
@@ -43,17 +54,26 @@ def readFileT2(file):
     tolm = float(f.readline())
 
     if(icod==1):
-      mtx.powerMet(aMatrix, tolm)
+      result = mtx.powerMet(aMatrix, tolm)
     elif(icod==2):
-      mtx.jacobiMet(aMatrix, tolm)
+      result = mtx.jacobiMet(aMatrix, tolm)
 
-def readFileT3(file):
+  return result
+
+def readTask3(file):
   with open(file, 'r') as f:
     icod = f.readline()
     n = f.readline()
     x = f.readline()
 
     if(icod==1):
-      mtx.interpolation()
+      result = mtx.interpolation()
     elif(icod==2):
-      mtx.regression()
+      result = mtx.regression()
+
+  return result
+
+def write(file, values):
+  with open(file, 'w') as f:
+    for value in values:
+      f.write(str(value)+"\n")
