fe16ad48c5d1f26064e4c797f451e60e67dab36a
Aline <alinej.capucho@gmail.com>
2022-06-26 18:06:02 -0300

    Add user.py file

--- src/user.py
+++ src/user.py
@@ -1,3 +1,29 @@
-import readLib as readLib
+import readLibTP2 as readLib
 
-readLib.runOperation('lista2.txt', 'result-lista2.txt', 2)
+# Testes - Tarefa 1
+# readLib.runOperation('test1-1-1.txt', 'result-test1-1-1.txt', 1)
+# readLib.runOperation('test1-1-2.txt', 'result-test1-1-2.txt', 1)
+# readLib.runOperation('test1-1-3.txt', 'result-test1-1-3.txt', 1)
+
+# readLib.runOperation('test1-2-1.txt', 'result-test1-2-1.txt', 1)
+# readLib.runOperation('test1-2-2.txt', 'result-test1-2-2.txt', 1)
+# readLib.runOperation('test1-2-3.txt', 'result-test1-2-3.txt', 1)
+
+
+# Testes - Tarefa 2
+# readLib.runOperation('test2-1-1.txt', 'result-test2-1-1.txt', 2)
+# readLib.runOperation('test2-1-2.txt', 'result-test2-1-2.txt', 2)
+
+# readLib.runOperation('test2-2-1.txt', 'result-test2-2-1.txt', 2)
+# readLib.runOperation('test2-2-2.txt', 'result-test2-2-2.txt', 2)
+
+# readLib.runOperation('test2-3-1.txt', 'result-test2-3-1.txt', 2)
+# readLib.runOperation('test2-3-2.txt', 'result-test2-3-2.txt', 2)
+# readLib.runOperation('test2-3-3.txt', 'result-test2-3-3.txt', 2)
+
+# readLib.runOperation('test2-4-1.txt', 'result-test2-4-1.txt', 2)
+
+
+# Testes - Tarefa 3
+# readLib.runOperation('test3-1-1.txt', 'result-test3-1-1.txt', 3)
+
